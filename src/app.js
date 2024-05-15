19e2be6a17a921b67958f1dd3e0ea67dd345af05
Arya Krisna Putra <aryakrisna07@gmail.com>
2024-05-15 20:41:41 +0700

    add quotes

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
 const todos = [
   {
     id: 1,
-    text: "Belajar Dasar HTML ",
+    text: 'Belajar Dasar HTML ',
     isComplete: true,
   },
   {
     id: 2,
-    text: "Belajar Advance HTML",
+    text: 'Belajar Advance HTML',
     isComplete: false,
   },
   {
     id: 3,
-    text: "Belajar Git",
+    text: 'Belajar Git',
     isComplete: false,
   },
   {
     id: 4,
-    text: "Belajar Basic Javascript",
+    text: 'Belajar Basic Javascript',
     isComplete: false,
   },
 ];
@@ -51,32 +51,32 @@ const handleRemoveButton = (id) => {
 };
 
 const createButtonEl = (text, className) => {
-  const buttonEl = document.createElement("button");
+  const buttonEl = document.createElement('button');
   buttonEl.innerText = text;
-  buttonEl.setAttribute("class", className);
+  buttonEl.setAttribute('class', className);
   return buttonEl;
 };
 
 const createTodoEL = (todo) => {
-  const todoEl = document.createElement("li");
-  const todoText = document.createElement("p");
+  const todoEl = document.createElement('li');
+  const todoText = document.createElement('p');
   todoText.innerText = todo.text;
 
-  const actionButtonContainer = document.createElement("div");
-  actionButtonContainer.setAttribute("class", "todo-button-container");
+  const actionButtonContainer = document.createElement('div');
+  actionButtonContainer.setAttribute('class', 'todo-button-container');
 
-  const doneButton = createButtonEl("Done", "btn btn-success");
-  doneButton.addEventListener("click", () => {
+  const doneButton = createButtonEl('Done', 'btn btn-success');
+  doneButton.addEventListener('click', () => {
     handleDoneButton(todo.id);
   });
 
-  const removeButton = createButtonEl("Remove", "btn btn-danger");
-  removeButton.addEventListener("click", () => {
+  const removeButton = createButtonEl('Remove', 'btn btn-danger');
+  removeButton.addEventListener('click', () => {
     handleRemoveButton(todo.id);
   });
 
-  const undoButton = createButtonEl("Undo", "btn btn-info");
-  undoButton.addEventListener("click", () => {
+  const undoButton = createButtonEl('Undo', 'btn btn-info');
+  undoButton.addEventListener('click', () => {
     handleUndoButton(todo.id);
   });
 
@@ -86,10 +86,10 @@ const createTodoEL = (todo) => {
 };
 
 const renderTodoList = () => {
-  const completedTodoList = document.querySelector(".completed-todo-list");
-  const uncompletedTodoList = document.querySelector(".uncompleted-todo-list");
-  completedTodoList.innerHTML = "";
-  uncompletedTodoList.innerHTML = "";
+  const completedTodoList = document.querySelector('.completed-todo-list');
+  const uncompletedTodoList = document.querySelector('.uncompleted-todo-list');
+  completedTodoList.innerHTML = '';
+  uncompletedTodoList.innerHTML = '';
 
   todos.forEach((todo) => {
     const todoEl = createTodoEL(todo);
@@ -107,18 +107,32 @@ const createPayload = (text) => {
 };
 
 const handleSubmitForm = () => {
-  const form = document.getElementById("todo-form");
-  form.addEventListener("submit", (e) => {
+  const form = document.getElementById('todo-form');
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const input = document.getElementById("todo-input");
+    const input = document.getElementById('todo-input');
     const payload = createPayload(input.value);
-    input.value = "";
+    input.value = '';
     todos.push(payload);
     renderTodoList();
   });
 };
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
   renderTodoList();
   handleSubmitForm();
+  const quoteButton = document.getElementById('quote-button');
+  const quoteContainer = document.getElementById('quote');
+  quoteButton.addEventListener('click', () => {
+    fetch('https://api.api-ninjas.com/v1/quotes?category=happiness', {
+      headers: {'X-Api-Key': 'tOj7dnA5FiVG9cekJkWjJg==xcyDJ9Uqe9HJonxS'},
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        quoteContainer.innerText = data[0].quote ?? 'Tidak ada quote hari ini';
+      })
+      .catch((err) => {
+        quoteContainer.innerText = 'Tidak ada quote hari ini';
+      });
+  });
 });
